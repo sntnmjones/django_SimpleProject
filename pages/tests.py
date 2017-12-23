81db9b83c9df7624c80f4cbeb99b98821180643b
sntnmjones <sntnmjones@gmail.com>
2017-12-23 15:07:46 -0800

    init commit

---
+++ pages/tests.py
@@ -0,0 +1,11 @@
+# /pages/tests.py
+from django.test import SimpleTestCase
+
+class SimpleTest(SimpleTestCase):
+    def test_home_page_status_code (self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_about_page_status_code (self):
+        response = self.client.get('/about/')
+        self.assertEquals(response.status_code, 200)
